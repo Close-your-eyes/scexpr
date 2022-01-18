94ee9ac87b62a2fb68b58308a08d0eb8a538cf97
Christopher Skopnik <christopher.skopnik@cohenfano.local>
2022-01-18 14:26:37 +0100

    up

--- R/convert_gene_identifier.R
+++ R/convert_gene_identifier.R
@@ -8,7 +8,7 @@
 #' GENENAME GENETYPE GO GOALL IPI MAP OMIM ONTOLOGY ONTOLOGYALL
 #' PATH PFAM PMID PROSITE REFSEQ SYMBOL UCSCKG UNIPROT
 #'
-#' @param gene_idents
+#' @param idents
 #' @param ident_in
 #' @param ident_out
 #' @param species
@@ -17,64 +17,64 @@
 #' @export
 #'
 #' @examples
-convert_gene_identifier <- function (gene_idents,
+convert_gene_identifier <- function (idents,
                                      ident_in = "SYMBOL",
                                      ident_out = "ENTREZID",
                                      species = c("Hs", "Mm")) {
 
+  #ident_out <-c("ENTREZID", "ENSEMBL")
+
   species <- match.arg(species, c("Hs", "Mm"))
+  ident_in <- match.arg(ident_in, AnnotationDbi::keytypes(my.db))
+  ident_out <- match.arg(ident_out, AnnotationDbi::keytypes(my.db), several.ok = T)
+
   if (species == "Hs") {
     if (!requireNamespace("org.Hs.eg.db", quietly = T)){
       BiocManager::install("org.Hs.eg.db")
     }
-    library("org.Hs.eg.db")
-    my.db <- org.Hs.eg.db
+    my.db <- org.Hs.eg.db::org.Hs.eg.db
+    idents <- gsub("^MT-", "MT", idents, ignore.case = F)
   }
   if (species == "Mm") {
     if (!requireNamespace("org.Mm.eg.db", quietly = T)){
       BiocManager::install("org.Mm.eg.db")
     }
-    library("org.Mm.eg.db")
-    my.db <- org.Mm.eg.db
+    my.db <- org.Mm.eg.db::org.Mm.eg.db
+    idents <- gsub("^mt-", "mt", idents, ignore.case = F)
   }
 
-  ident_in <- match.arg(ident_in, keytypes(my.db))
-  ident_out <- match.arg(ident_out, keytypes(my.db), several.ok = T)
+  start_len <- length(idents)
+  idents <- suppressMessages(AnnotationDbi::select(my.db, keys = as.character(unique(idents)), keytype = ident_in, column = ident_out))
 
-  gene_idents <- unique(gene_idents)
-  gene_idents <- gsub("^MT-", "MT", gene_idents, ignore.case = T)
-  gene_idents <- AnnotationDbi::select(my.db, keys = as.character(gene_idents), keytype = ident_in, column = ident_out)
 
-  # manual filtering of previously recognized duplicate matchings
-  if ("SYMBOL" %in% names(gene_idents) && "ENTREZID" %in% names(gene_idents)) {
-    gene_idents <-
-      gene_idents %>%
-      dplyr::filter(!(SYMBOL == "MEMO1" & ENTREZID == "7795")) %>%
-      dplyr::filter(!(SYMBOL == "TEC" & ENTREZID == "100124696")) %>%
-      dplyr::filter(!(SYMBOL == "MMD2" & ENTREZID == "100505381")) %>%
-      dplyr::filter(!(SYMBOL == "HBD" & ENTREZID == "100187828"))
-    print("Duplicated matches:")
-    gene_idents[which(gene_idents$SYMBOL %in% gene_idents[which(duplicated(gene_idents$SYMBOL)), "SYMBOL"]), ]
+  if (ident_in == "SYMBOL" && "ENTREZID" %in% ident_out) {
+    # special treatment
+    # yet wrong, fix!
+    idents <- idents[intersect(which(!idents$SYMBOL == "MEMO1"), which(!idents$ENTREZID == "7795")),]
+    idents <- idents[intersect(which(!idents$SYMBOL == "TEC"), which(!idents$ENTREZID == "100124696")),]
+    idents <- idents[intersect(which(!idents$SYMBOL == "MMD2"), which(!idents$ENTREZID == "100505381")),]
+    idents <- idents[intersect(which(!idents$SYMBOL == "HBD"), which(!idents$ENTREZID == "100187828")),]
+    if (any(duplicated(idents[,ident_in]))) {
+      print(paste0("Duplicate return for: ", paste(idents[,ident_in][which(duplicated(idents[,ident_in]))], collapse = ", ")))
+      print("Made distinct with dplyr::distinct")
+      idents <- dplyr::distinct(idents, !!sym(ident_in), .keep_all = T)
+    }
+    idents$ALIAS <- limma::alias2SymbolTable(alias = idents$SYMBOL, species = species)
+    rows <- intersect(which(is.na(idents$ENTREZID)), which(!is.na(idents$ALIAS)))
+    if (length(rows) > 0) {
+      idents[rows, "ENTREZID"] <- AnnotationDbi::select(my.db, keys = as.character(idents[rows, "ALIAS"]), keytype = ident_in, column = "ENTREZID")$ENTREZID
+    }
+
+  } else {
+    if (any(duplicated(idents[,ident_in]))) {
+      print(paste0("Duplicate return for: ", paste(idents[,ident_in][which(duplicated(idents[,ident_in]))], collapse = ", ")))
+      print("Made distinct with dplyr::distinct")
+      idents <- dplyr::distinct(idents, !!sym(ident_in), .keep_all = T)
+    }
   }
-
-  if ("SYMBOL" %in% names(gene_idents)) {
-    gene_idents$ALIAS <- limma::alias2SymbolTable(alias = gene_idents$SYMBOL, species = "Hs")
+  idents$GENENAME <- dplyr::distinct(suppressMessages(AnnotationDbi::select(my.db, keys = idents[,ident_in], keytype = ident_in, column = "GENENAME")))
+  if (nrow(idents) != start_len) {
+    print("input length and output length are not identical.")
   }
-  if ("SYMBOL" %in% names(gene_idents) && "ENTREZID" %in% names(gene_idents)) {
-    gene_idents[intersect(which(is.na(gene_idents$ENTREZID)), which(!is.na(gene_idents$ALIAS))), "ENTREZID"] <- AnnotationDbi::select(my.db, keys = as.character(gene_idents[intersect(which(is.na(gene_idents$ENTREZID)), which(!is.na(gene_idents$ALIAS))), "ALIAS"]), keytype = ident_in, column = ident_out)$ENTREZID
-
-  }
-
-  print("Duplicated ENTREZIDs are filtered for distinct rows of ENTREZID and ALIAS:")
-  gene_idents[which(gene_idents$ENTREZID %in% gene_idents[which(duplicated(gene_idents$ENTREZID, incomparables = NA)), "ENTREZID"]), ]
-  print("Rows for which an ALIAS was found but no ENTREZID:")
-  gene_idents[intersect(which(is.na(gene_idents$ENTREZID)), which(!is.na(gene_idents$ALIAS))), ]
-  print("Rows with ENTREZID == NA will also be dropped")
-
-  gene_idents <- dplyr::distinct(gene_idents, ENTREZID, ALIAS, .keep_all = T)
-  gene_idents <- tidyr::drop_na(gene_idents, ENTREZID)
-  gene_idents <- dplyr::left_join(gene_idents, AnnotationDbi::select(my.db, keys = as.character(gene_idents[which(!is.na(gene_idents$ALIAS)), "ALIAS"]), keytype = "SYMBOL", column = "GENENAME"), by = c("ALIAS" = "SYMBOL"))
-  gene_idents <- dplyr::distinct(gene_idents, SYMBOL, ENTREZID, ALIAS, .keep_all = T)
-
-  return(gene_idents)
+  return(idents)
 }
