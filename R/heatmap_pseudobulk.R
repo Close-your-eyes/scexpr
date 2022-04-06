8de5d83e16eaca746c66c7d307183e1c60a50ef2
Christopher Skopnik <christopher.skopnik@cohenfano.local>
2022-04-06 21:49:09 +0200

    up

--- R/heatmap_pseudobulk.R
+++ R/heatmap_pseudobulk.R
@@ -136,10 +136,12 @@ heatmap_pseudobulk <- function(SO,
     SO <- subset(SO, cells = rownames(SO@meta.data[,meta.col,drop=F][which(SO@meta.data[,meta.col] %in% levels.calc),,drop=F]))
   }
 
+  ## presto gives deviating results with respect to avgExpr and logFC (maybe due to approximation which makes calculation faster)
+  ## nevertheless, in order to select marker genes by logFC or other statistics the presto output is useful
+  wil_auc <- presto::wilcoxauc(SO, meta.col, seurat_assay = assay)
+  wil_auc <- wil_auc[which(wil_auc$feature %in% features),]
+
   if (missing(features)) {
-    ## presto gives deviating results with respect to avgExpr and logFC (maybe due to approximation which makes calculation faster)
-    ## nevertheless, in order to select marker genes by logFC or other statistics the presto output is useful
-    wil_auc <- presto::wilcoxauc(SO, meta.col, seurat_assay = assay)
     topn.metric <- match.arg(topn.metric, c("logFC", "auc", "padj"))
 
     features <-
