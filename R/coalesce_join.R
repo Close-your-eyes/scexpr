ac73f042c474d7288d4e117425176215bf6da636
Close-your-eyes <vonskopnik@pm.me>
2022-05-15 21:00:32 +0200

    coalesce join!!!

---
+++ R/coalesce_join.R
@@ -0,0 +1,44 @@
+#' Title
+#'
+#' @param x
+#' @param y
+#' @param by
+#' @param suffix
+#' @param join
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+coalesce_join <- function(x, y,
+                          by = NULL, suffix = c(".x", ".y"),
+                          join = dplyr::full_join, ...) {
+
+  # copied originally from: https://alistaire.rbind.io/blog/coalescing-joins/
+  # ideas:
+  # https://stackoverflow.com/questions/33954292/merge-two-data-frame-and-replace-the-na-value-in-r#33954334
+  # https://community.rstudio.com/t/merging-2-dataframes-and-replacing-na-values/32123/2
+  # https://github.com/WinVector/rqdatatable
+
+  joined <- join(x, y, by = by, suffix = suffix, ...)
+  # names of desired output
+  cols <- union(names(x), names(y))
+
+  to_coalesce <- names(joined)[!names(joined) %in% cols]
+  suffix_used <- suffix[ifelse(endsWith(to_coalesce, suffix[1]), 1, 2)]
+  # remove suffixes and deduplicate
+  to_coalesce <- unique(substr(
+    to_coalesce,
+    1,
+    nchar(to_coalesce) - nchar(suffix_used)
+  ))
+
+  coalesced <- purrr::map_dfc(to_coalesce, ~dplyr::coalesce(
+    joined[[paste0(.x, suffix[1])]],
+    joined[[paste0(.x, suffix[2])]]
+  ))
+  names(coalesced) <- to_coalesce
+
+  dplyr::bind_cols(joined, coalesced)[cols]
+}
