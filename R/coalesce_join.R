1ae8c03445e1d61d1f226fc38072f0aa16dcc997
Christopher Skopnik <christopher.skopnik@MAC20220426.drfz.de>
2022-05-16 10:55:03 +0200

    coalesce ignore case

--- R/coalesce_join.R
+++ R/coalesce_join.R
@@ -34,19 +34,24 @@ coalesce_join <- function(x, y,
   cols <- union(names(x), names(y))
 
   to_coalesce <- names(joined)[!names(joined) %in% cols]
-  suffix_used <- suffix[ifelse(endsWith(to_coalesce, suffix[1]), 1, 2)]
-  # remove suffixes and deduplicate
-  to_coalesce <- unique(substr(
-    to_coalesce,
-    1,
-    nchar(to_coalesce) - nchar(suffix_used)
-  ))
+  if (length(to_coalesce) > 0) {
+    suffix_used <- suffix[ifelse(endsWith(to_coalesce, suffix[1]), 1, 2)]
+    # remove suffixes and deduplicate
+    to_coalesce <- unique(substr(
+      to_coalesce,
+      1,
+      nchar(to_coalesce) - nchar(suffix_used)
+    ))
 
-  coalesced <- purrr::map_dfc(to_coalesce, ~dplyr::coalesce(
-    joined[[paste0(.x, suffix[1])]],
-    joined[[paste0(.x, suffix[2])]]
-  ))
-  names(coalesced) <- to_coalesce
+    coalesced <- purrr::map_dfc(to_coalesce, ~dplyr::coalesce(
+      joined[[paste0(.x, suffix[1])]],
+      joined[[paste0(.x, suffix[2])]]
+    ))
+    names(coalesced) <- to_coalesce
 
-  dplyr::bind_cols(joined, coalesced)[cols]
+    dplyr::bind_cols(joined, coalesced)[cols]
+  } else {
+    joined
+  }
+
 }
