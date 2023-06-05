877226ffd3c2d55722c04f143994772ebe5b969e
Christopher Skopnik <christopher.skopnik@MAC20220426.drfz.de>
2023-06-05 09:01:27 +0200

    up

--- R/fsgea_on_msigdbr.R
+++ R/fsgea_on_msigdbr.R
@@ -1,63 +1,81 @@
 #' Title
 #'
 #' @param gene.ranks
-#' @param use.msigdbr
 #' @param gene.sets
 #' @param min.padj
-#' @param ...
+#' @param use.msigdbr
+#' @param msigdbr_args
+#' @param fgsea_fun
+#' @param fgsea_args
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fgsea_on_msigdbr <- function(gene.ranks,
-                             use.msigdbr = T,
+fgsea_on_msigdbr <- function(gene.ranks = NULL,
                              gene.sets = NULL,
                              min.padj = 0.001, # which plots to generate
-                             ...) {
+                             use.msigdbr = T,
+                             msigdbr_args = list(species = "Homo sapiens", category = NULL, subcategory = NULL),
+                             fgsea_fun = fgsea::fgseaMultilevel,
+                             fgsea_args = list(stats = gene.ranks, pathways = gene.sets)) {
 
   if (!requireNamespace("fgsea", quietly = TRUE)) {
     BiocManager::install("fgsea")
   }
 
-  if (length(gene.ranks) == 0) {
-    return(NULL)
+  if (is.null(gene.ranks)) {
+    stop("gene.ranks has to be provided.")
   }
-
-  dots <- list(...)
-  if (any(c("pathways", "stats") %in% names(dots))) {
-    stop("Please do not provide pathways or stats in ...")
+  if (!is.numeric(gene.ranks)) {
+    stop("gene.ranks has to be a numeric metric.")
   }
-
+  if (is.null(names(gene.ranks))) {
+    stop("gene.ranks has to have names which should be genes.")
+  }
   if (!is.null(gene.sets)) {
     if (!is.list(gene.sets) || is.null(names(gene.sets))) {
-      stop("gene.sets has to be a named list of gene sets. See examples as returned from msigdbr::msigdbr splitted to list by gene_symbol and gs_name.")
+      stop("gene.sets has to be a named list of gene sets.")
     }
   }
+  fgsea_fun <- match.fun(fgsea_fun)
 
   if (use.msigdbr) {
     if (!requireNamespace("msigdbr", quietly = T)) {
       utils::install.packages("msigdbr")
     }
-    sets <- as.data.frame(do.call(msigdbr::msigdbr, args = dots[which(names(dots) %in% names(formals(msigdbr::msigdbr)))])[,c("gs_name", "gene_symbol")])
-    sets <- split(sets$gene_symbol, sets$gs_name)
+    gene.sets <- c(gene.sets, .get.split.msigdbr(msigdbr_args = msigdbr_args))
   }
-  if (!is.null(gene.sets)) {
-    sets <- c(gene.sets, sets)
-    names(sets) <- make.unique(names(sets))
-  }
+  names(gene.sets) <- make.unique(names(gene.sets))
 
-  results <- do.call(fgsea::fgseaMultilevel, args = c(unlist(dots[which(names(dots) %in% names(formals(fgsea::fgseaMultilevel)))]),
-                                                      list(pathways = sets,
-                                                           stats = gene.ranks)))
-  results_signif <- results[which(results$padf <= min.padj)]
-  gsea_plots <- lapply(results_signif$pathway, function(x) {
-    fgsea::plotEnrichment(pathway = sets[[x]], stats = gene.ranks)
+
+  if (!"pathways" %in% names(fgsea_args)) {
+    fgsea_args <- c(list(pathways = gene.sets), fgsea_args)
+  }
+  if (!"stats" %in% names(fgsea_args)) {
+    fgsea_args <- c(list(stats = gene.ranks), fgsea_args)
+  }
+  results <- as.data.frame(Gmisc::fastDoCall(fgsea_fun, args = fgsea_args))
+
+  gsea_plots <- lapply(.self_naming(results[which(results$padj <= min.padj),"pathway",drop=T]), function(x) {
+    fgsea::plotEnrichment(pathway = gene.sets[[x]], stats = gene.ranks) +
+      labs(title = x, subtitle = paste0("p = ", signif(results[which(results$pathway == x), "padj"], 2)))
   })
-  names(gsea_plots) <- results_signif$pathway
 
   return(list(fgsea_table = results,
               fgsea_plots = gsea_plots,
-              gene.sets = sets,
+              gene.sets = gene.sets,
               gene.ranks = gene.ranks))
 }
+
+
+.get.split.msigdbr <- function(msigdbr_args = list(species = "Homo sapiens", category = NULL, subcategory = NULL)) {
+  sets <- Gmisc::fastDoCall(msigdbr::msigdbr, args = msigdbr_args)[,c("gs_name", "gene_symbol")]
+  sets <- split(sets$gene_symbol, sets$gs_name)
+  return(sets)
+}
+
+.self_naming <- function(x) {
+  return(stats::setNames(x, x))
+}
+
