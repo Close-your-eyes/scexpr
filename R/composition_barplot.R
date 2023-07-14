70d34d205c1ec1ad0576ecf747530503621f6ddd
Christopher Skopnik <christopher.skopnik@MAC20220426.drfz.de>
2023-07-14 14:15:52 +0200

    up

--- R/composition_barplot.R
+++ R/composition_barplot.R
@@ -4,11 +4,20 @@
 #' @param x_cat
 #' @param fill_cat
 #' @param border_color
-#' @param labels
-#' @param min_label
-#' @param in_percent
-#' @param label_decimals
+#' @param plot_rel_labels
+#' @param min_label_freq
+#' @param label_rel_pct
+#' @param label_rel_pct_decimals
 #' @param col_pal
+#' @param plot_abs_labels
+#' @param summarize_all_x
+#' @param label_rel_nudge
+#' @param label_abs_nudge
+#' @param flip
+#' @param label_size
+#' @param plot_total_labels
+#' @param total_labels_ypos
+#' @param plot_sigma
 #'
 #' @return
 #' @export
@@ -19,10 +28,20 @@ composition_barplot <- function(SO,
                                 fill_cat,
                                 col_pal = scexpr::col_pal("custom"),
                                 border_color = "black",
-                                labels = F,
-                                min_label = 0.05,
-                                in_percent = T,
-                                label_decimals = 2) {
+                                plot_rel_labels = F,
+                                plot_abs_labels = F,
+                                plot_total_labels = F,
+                                total_labels_ypos = 1.05,
+                                plot_sigma = F,
+                                sigma_just = ifelse(flip, 2, -1),
+                                min_label_freq = 0.05,
+                                label_rel_pct = T,
+                                label_rel_pct_decimals = 2,
+                                summarize_all_x = F,
+                                label_rel_nudge = c(0,0),
+                                label_abs_nudge = c(0,0),
+                                label_size = 4,
+                                flip = F) {
 
 
   if (methods::is(SO, "Seurat")) {
@@ -36,7 +55,7 @@ composition_barplot <- function(SO,
     stop("fill_cat not found in SO.")
   }
 
-  if (in_percent) {
+  if (label_rel_pct) {
     fctr <- 100
   } else {
     fctr <- 1
@@ -48,43 +67,106 @@ composition_barplot <- function(SO,
     dplyr::left_join(dplyr::count(SO, !!rlang::sym(x_cat), name = "x_total"), by = x_cat) %>%
     dplyr::left_join(dplyr::count(SO, !!rlang::sym(fill_cat), name = "fill_total"), by = fill_cat) %>%
     dplyr::mutate(rel_x = n/x_total) %>%
-    dplyr::mutate(rel_fill = n/fill_total)
+    dplyr::mutate(rel_fill = n/fill_total) # rel_fill is not used below
+
+  if (summarize_all_x) {
+    table <- dplyr::bind_rows(table,
+                              SO %>%
+                                dplyr::count(!!rlang::sym(fill_cat)) %>%
+                                dplyr::mutate(x_total = nrow(SO)) %>%
+                                dplyr::mutate(fill_total = nrow(SO)) %>%
+                                dplyr::mutate(rel_x = n/x_total) %>%
+                                dplyr::mutate(rel_fill = n/fill_total) %>%
+                                dplyr::mutate(!!x_cat := "all"))
+  }
+
+  if (plot_total_labels) {
+    table0 <-
+      SO %>%
+      dplyr::count(!!rlang::sym(x_cat))
+
+    if (summarize_all_x) {
+      temp <- data.frame(x_cat = "all", n = nrow(SO))
+      names(temp)[1] <- x_cat
+      table0 <- dplyr::bind_rows(table0, temp)
+      table0$label_ypos <- total_labels_ypos
+      table0$label_color = "black" # could be made relative to background color
+    }
+  }
+
+
+  if (!is.null(names(col_pal)) && any(!unique(table[,fill_cat,drop=T]) %in% names(col_pal))) {
+    col_pal <- unname(col_pal)
+  } else {
+    table$bar_segment_cols <- col_pal[as.character(table[,fill_cat,drop=T])]
+    table$label_color <- ifelse(farver::decode_colour(table$bar_segment_cols, to = "hcl")[, "l"] > 50, "black", "white")
+  }
+
 
 
   ## use cumsum approach to define label position in middle of bars
-  ## this allow to remove label by min_label and still have the correct coordinate
+  ## this allow to remove label by min_label_freq and still have the correct coordinate
   ## coordinates became wrong with position = ggplot2::position_stack(vjust = 0.5) when some values were removed
-  label_table <-
+  table <-
     table %>%
     dplyr::group_by(!!rlang::sym(x_cat)) %>%
     dplyr::arrange(desc(!!rlang::sym(fill_cat)), .by_group = T) %>%
     dplyr::mutate(rel_x_cumsum = cumsum(rel_x)) %>%
     dplyr::mutate(rel_x_cumsum_lag = dplyr::lag(rel_x_cumsum, default = 0)) %>%
     dplyr::mutate(label_ypos = rel_x_cumsum_lag + (rel_x_cumsum-rel_x_cumsum_lag)/2) %>%
-    dplyr::filter(rel_x >= min_label) %>%
-    dplyr::mutate(rel_x = rel_x*fctr) %>%
-    dplyr::mutate(label_ypos = label_ypos*fctr) %>%
+    #dplyr::filter(rel_x >= min_label_freq) %>%
+    #dplyr::mutate(rel_x = rel_x*fctr) %>%
+    #dplyr::mutate(label_ypos = label_ypos*fctr) %>%
     tibble::as_tibble()
 
-  table <-
-    table %>%
-    dplyr::mutate(rel_x = rel_x*fctr) %>%
-    tibble::as_tibble()
-
-  if (!is.null(names(col_pal)) && any(!unique(table[,fill_cat,drop=T]) %in% names(col_pal))) {
-    col_pal <- unname(col_pal)
-  }
 
   plot <-
-    ggplot2::ggplot(table, ggplot2::aes(x = !!rlang::sym(x_cat), y = rel_x, fill = !!rlang::sym(fill_cat))) +
+    ggplot2::ggplot(table, ggplot2::aes(x = !!rlang::sym(x_cat), y = rel_x*fctr, fill = !!rlang::sym(fill_cat))) +
     ggplot2::geom_col(color = border_color) +
-    ggplot2::scale_fill_manual(values = col_pal)
+    ggplot2::scale_fill_manual(values = col_pal) +
+    ggplot2::labs(y = ifelse(label_rel_pct, "frequency [%]", "frequency")) +
+    scale_y_continuous(breaks = c(0,0.25,0.5,0.75,1)*fctr)
 
-
-  if (labels) {
-    plot <- plot + ggplot2::geom_text(data = label_table, ggplot2::aes(label = round(rel_x, label_decimals), x = !!rlang::sym(x_cat), y = label_ypos))
+  if (plot_rel_labels) {
+    plot <-
+      plot +
+      ggplot2::geom_text(data = table %>% dplyr::filter(rel_x >= min_label_freq),
+                         ggplot2::aes(color = I(label_color), label = paste0(round(rel_x*fctr, label_rel_pct_decimals), " %"), x = !!rlang::sym(x_cat), y = label_ypos*fctr),
+                         nudge_x = label_rel_nudge[1], nudge_y = label_rel_nudge[2],
+                         size = label_size)
     #position = ggplot2::position_stack(vjust = 0.5))
   }
 
-  return(list(table = table, plot = plot))
+  if (plot_abs_labels) {
+    plot <-
+      plot +
+      ggplot2::geom_text(data = table %>% dplyr::filter(rel_x >= min_label_freq),
+                         ggplot2::aes(color = I(label_color), label = n, x = !!rlang::sym(x_cat), y = label_ypos*fctr),
+                         nudge_x = label_abs_nudge[1], nudge_y = label_abs_nudge[2],
+                         size = label_size)
+    #position = ggplot2::position_stack(vjust = 0.5))
+  }
+
+  if (plot_total_labels) {
+    plot <-
+      plot +
+      ggplot2::geom_text(data = table0,
+                         ggplot2::aes(color = I(label_color), label = n, x = !!rlang::sym(x_cat), y = label_ypos*fctr),
+                         size = label_size, inherit.aes = F) #
+  }
+
+  if (flip) {
+    plot <-
+      plot +
+      ggplot2::coord_flip()
+    if (plot_sigma) {
+      plot <- plot + annotate("text", label = "\u03A3", x = Inf, y = total_labels_ypos*fctr, vjust = sigma_just, size = label_size)
+    }
+  } else {
+    if (plot_sigma) {
+      plot <- plot + annotate("text", label = "\u03A3", x = -Inf, y = total_labels_ypos*fctr, hjust = sigma_just, size = label_size)
+    }
+  }
+
+  return(list(data = table, plot = plot))
 }
