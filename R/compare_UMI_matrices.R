58c4c692060e31bc17fb821f847adbb2c2247ec0
Close-your-eyes <vonskopnik@pm.me>
2024-02-10 23:41:38 +0100

    up

--- R/compare_UMI_matrices.R
+++ R/compare_UMI_matrices.R
@@ -22,6 +22,8 @@
 #' }
 compare_UMI_matrices <- function(mat1, mat2) {
 
+  # how to work completely on sparse matrices?
+
   if (is.null(colnames(mat1)) || is.null(colnames(mat2)) || is.null(rownames(mat1)) || is.null(rownames(mat2))) {
     stop("mat1 and mat2 need to have rows.")
   }
@@ -33,9 +35,15 @@ compare_UMI_matrices <- function(mat1, mat2) {
 
   abs_diff <- mat1 - mat2
   squ_diff <- abs_diff^2
-  rel_diff <- (abs(abs_diff)+abs(abs_diff))/(mat1+mat2)
+  rel_diff <- (abs(abs_diff)+abs(abs_diff))/(mat1 + mat2)
   # mean squared error
-  mse <- mean(squ_diff)
+
+  mse <- Matrix::mean(squ_diff)
+
+  # this avoids coercion to dense matrix, but is it always correct?
+  # mean(c(mean(1:10), mean(11:20))) == mean(1:20)
+  #mse <- mean(Matrix::colMeans(squ_diff))
+
   # root mean squared error (RMSE)
   rmse <- sqrt(mse)
 
@@ -49,21 +57,24 @@ compare_UMI_matrices <- function(mat1, mat2) {
   #total_entries <- ncol(mat1)*nrow(mat1) # same length(mat1)
   rel_entry_diff <- abs_entry_diff/length(mat1)
 
-  # _r for row
-  rsme_r <- utils::stack(purrr::map_dbl(setNames(1:nrow(mat1), rownames(mat1)), function(x) rsme(mat1[x,], mat2[x,])))
-  names(rsme_r)[1] <- "RSME"
 
-  abs_umi_r <- utils::stack(purrr::map_dbl(setNames(1:nrow(mat1), rownames(mat1)), function(x) abs_umi(mat1[x,], mat2[x,])))
+  # _r for row
+  abs_umi_r <- utils::stack(rowSums(mat1+mat2)/2)
   names(abs_umi_r)[1] <- "abs_umi"
 
-  abs_err_r <- utils::stack(purrr::map_dbl(setNames(1:nrow(mat1), rownames(mat1)), function(x) abs_err(mat1[x,], mat2[x,])))
+  abs_err_r <- utils::stack(rowSums(mat1-mat2))
   names(abs_err_r)[1] <- "abs_err"
 
-  abs_abs_err_r <- utils::stack(purrr::map_dbl(setNames(1:nrow(mat1), rownames(mat1)), function(x) abs_abs_err(mat1[x,], mat2[x,])))
+  abs_abs_err_r <- utils::stack(rowSums(abs(mat1-mat2)))
   names(abs_abs_err_r)[1] <- "total_diff"
 
-  rel_abs_err_r <- utils::stack(purrr::map_dbl(setNames(1:nrow(mat1), rownames(mat1)), function(x) rel_abs_err(mat1[x,], mat2[x,])))
+  rel_abs_err_r <- utils::stack(stats::setNames(abs_abs_err_r$total_diff/abs_umi_r$abs_umi, abs_abs_err_r$ind))
   names(rel_abs_err_r)[1] <- "rel_total_diff"
+
+  # purrr::map_dbl(setNames(1:nrow(mat1), rownames(mat1)), function(x) rsme(mat1[x,], mat2[x,]), .progress = T)
+  rsme_r <- utils::stack(sqrt(Matrix::rowMeans((mat1-mat2)^2)))
+  names(rsme_r)[1] <- "RSME"
+
 
   rowwise_error_df <- purrr::reduce(list(rsme_r,
                                          abs_umi_r,
@@ -76,25 +87,28 @@ compare_UMI_matrices <- function(mat1, mat2) {
   rowwise_error_df$rowname <- as.character(rowwise_error_df$rowname)
 
   rowwise_error_df$abs_umi_group = cut(rowwise_error_df$abs_umi,
-                         include.lowest = T,
-                         breaks = c(1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8))
+                                       include.lowest = T,
+                                       breaks = c(1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8))
   levels <- c(0, levels(rowwise_error_df$abs_umi_group))
   rowwise_error_df$abs_umi_group <- as.character(rowwise_error_df$abs_umi_group)
   rowwise_error_df$abs_umi_group[which(is.na(rowwise_error_df$abs_umi_group))] <- "0"
   rowwise_error_df$abs_umi_group <- factor(rowwise_error_df$abs_umi_group, levels = levels)
 
+  matrixwise_error_df <- data.frame(RSME = rmse,
+                                    #MSE = mse,
+                                    total_diff = total_diff,
+                                    rel_diff = rel_diff,
+                                    abs_unequal_entry = abs_entry_diff,
+                                    rel_unequal_entry = rel_entry_diff)
 
-  return(list(RSME = rmse,
-              MSE = mse,
-              total_diff = total_diff,
-              rel_diff = rel_diff,
-              abs_unequal_entry = abs_entry_diff,
-              rel_unequal_entry = rel_entry_diff,
+  return(list(matrixwise_error = matrixwise_error_df,
               rowwise_error = rowwise_error_df))
 
 }
 
 
+
+## obsolete funs: very slow.
 
 rsme <- function(x, y) {
   squared_diff <- (x - y)^2
